99a0ab10198fe612aad5a809ab93ed12e49ba556
Zoe Cutler <36316793+zcutlerf@users.noreply.github.com>
2023-05-24 16:38:25 -0400

    Initial Commit

---
+++ WKTtoMapKit/WKTtoMapKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WKTtoMapKitApp.swift
+//  WKTtoMapKit
+//
+//  Created by Zoe Cutler on 5/24/23.
+//
+
+import SwiftUI
+
+@main
+struct WKTtoMapKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
